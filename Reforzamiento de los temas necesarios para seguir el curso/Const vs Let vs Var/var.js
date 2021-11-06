4792e703ad6eaf9727bc06c51ee88a0a6f38e923
José Manuel López Ortiz <josemanuel18x@gmail.com>
2021-11-06 20:16:13 +0100

    Creación segunda sección curso + archivo txt con los temas de la sección & Lección Const vs Let vs Var

---
+++ Reforzamiento de los temas necesarios para seguir el curso/Const vs Let vs Var/var.js
@@ -0,0 +1,20 @@
+
+// var nombre = 'Wolverine';
+
+// if (true) {
+//     var nombre = 'Magneto';
+// }
+
+// console.log(nombre);
+
+nombre = 'Wolverine';
+
+if (true) {
+    nombre = 'Magneto';
+}
+
+// var nombre;
+
+console.log (nombre);
+
+var nombre;
