22a36ad172f8ed387fdfc5f5b0a71adf69eb651a
José Manuel López Ortiz <josemanuel18x@gmail.com>
2021-11-06 18:50:10 +0100

    Ciclo de eventos de un proceso en Node & Instalación de Nodemon

---
+++ Fundamentos de Node/Ciclo de vida de un proceso en Node/app1.js
@@ -0,0 +1,10 @@
+
+let nombre = 'José Manuel';
+
+console.log('Hola ' + nombre);
+
+
+let a = 10;
+let b = 20;
+
+console.log(a + b);
---
+++ Fundamentos de Node/Nodemon/app.js
@@ -0,0 +1,19 @@
+
+let nombre = 'Fernando';
+console.log(nombre);
+
+nombre = 'José Manuel';
+console.log(nombre);
+
+///////////////////////////////////////////////////////
+
+const nombre1 = 'Fernando';
+console.log(nombre1);
+
+
+const nombre2 = 'José Manuel';
+console.log(nombre2);
+
+///////////////////////////////////////////////////////
+
+console.log('Hola ' + nombre);
