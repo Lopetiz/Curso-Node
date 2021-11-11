b944bb8ae4189be24a0353ba987ba3336922e345
José Manuel López Ortiz <josemanuel18x@gmail.com>
2021-11-12 00:43:39 +0100

    Lecciones de Requerir paquetes - require & Importar archivos de nuestro proyecto

---
+++ Bases de Node/Importar archivos de nuestro proyecto/app.js
@@ -0,0 +1,10 @@
+
+const { crearArchivo } = require('./multiplicar');
+
+console.clear();
+
+const base = 5;
+
+crearArchivo( base )
+    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+    .catch ( err => console.log(err) );
