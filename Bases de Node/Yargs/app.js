796e73a3d7352a905fbc90e93aaa0d11beabd50c
José Manuel López Ortiz <josemanuel18x@gmail.com>
2021-11-12 00:48:16 +0100

    Lecciones de Yargs & Configuración de Yargs

---
+++ Bases de Node/Yargs/app.js
@@ -0,0 +1,15 @@
+
+const { crearArchivo } = require('./multiplicar');
+const argv = require('yargs').argv;
+
+console.clear();
+
+console.log( process.argv );
+console.log( argv );
+
+console.log('base: yargs', argv.base );
+// const base = 5;
+
+// crearArchivo( base )
+//     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+//     .catch ( err => console.log(err) );
