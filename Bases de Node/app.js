4124ccaef3721ce589b09b4d0251c84d4f30bfc0
José Manuel López Ortiz <josemanuel18x@gmail.com>
2021-11-12 01:47:41 +0100

    Lección de Yargs

--- Bases de Node/app.js
+++ Bases de Node/app.js
@@ -1,13 +1,15 @@
 
 const { crearArchivo } = require('./multiplicar');
+const argv = require('yargs').argv;
 
 console.clear();
 
-const [ , , arg3 = 'base=5' ] = process.argv;
-const [ , base = 5 ] = arg3.split('=');
+console.log( process.argv );
+console.log( argv );
 
+console.log('base: yargs', argv.base );
 // const base = 5;
 
-crearArchivo( base )
-    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-    .catch ( err => console.log(err) );
+// crearArchivo( base )
+//     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+//     .catch ( err => console.log(err) );
