a58136fc114ac170f6a46eb55a0b4e503fc7bf29
José Manuel López Ortiz <josemanuel18x@gmail.com>
2021-12-03 18:34:04 +0100

    Introducción a Express & Servir contenido estático

--- Webserver/app.js
+++ Webserver/app.js
@@ -1,19 +1,19 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080;
+ 
+// Servir contenido estático
+app.use( express.static('public') );
 
-http.createServer( (req, res) => {
 
-    // res.writeHead(200, { 'Content-Type': 'application/json'})
-    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
-    res.writeHead(200, { 'Content-Type': 'application/csv' });
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola mundo en ruta')
+});
 
-    res.write( 'id, nombre\n' );
-    res.write( '1, Jose Manuel\n' );
-    res.write( '2, Noelia\n' );
-    res.write( '3, Abel\n' );
-    res.write( '4, Laura\n' );
-    res.end();
-
-} )
-.listen( 8080 );
-
-console.log('Escuchando el puerto', 8080);
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html')
+});
+ 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
