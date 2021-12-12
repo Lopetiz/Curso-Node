2b3c228929a9b8757b2ddd744f99d1470ce346f1
José Manuel López Ortiz <josemanuel18x@gmail.com>
2021-12-12 02:37:58 +0100

    Obtener datos de un POST & Parámetros de segmento y query

--- REST Server/controllers/usuarios.js
+++ REST Server/controllers/usuarios.js
@@ -1,30 +1,47 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
-const usuariosGet = (req, res) => {
+const usuariosGet = (req = request, res = response) => {
+
+    const {q, nombre = 'No name', apikey, page = '1', limit} = req.query;
+
     res.json({
-        msg: 'get API - controlador'
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
     });
 };
 
-const usuariosPost = (req, res) => {
+const usuariosPost = (req, res = response) => {
+    
+    const { nombre, edad } = req.body;
+
     res.json({
-        msg: 'post API - controlador'
+        msg: 'post API - controlador',
+        nombre,
+        edad
     });
 };
 
-const usuariosPut = (req, res) => {
+const usuariosPut = (req, res = response) => {
+    
+    const { id } = req.params;
+
     res.json({
-        msg: 'put API - controlador'
+        msg: 'put API - controlador',
+        id
     });
 };
 
-const usuariosPatch = (req, res) => {
+const usuariosPatch = (req, res = response) => {
     res.json({
         msg: 'patch API - controlador'
     });
 };
 
-const usuariosDelete = (req, res) => {
+const usuariosDelete = (req, res = response) => {
     res.json({
         msg: 'delete API - controlador'
     });
